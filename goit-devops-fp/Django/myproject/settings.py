cd0bbbf07f6855c25317cbaf0bbd9f64fa462a07
EssenceMaks <makski.wish@gmail.com>
2026-02-17 06:49:02 +0200

    Add django-prometheus metrics and ServiceMonitor

--- goit-devops-fp/Django/myproject/settings.py
+++ goit-devops-fp/Django/myproject/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_prometheus',
     'app',
 ]
 
@@ -27,6 +28,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_prometheus.middleware.PrometheusAfterMiddleware',
 ]
 
 ROOT_URLCONF = 'myproject.urls'
